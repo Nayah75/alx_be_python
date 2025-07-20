45f98679fd5620e4be2053d686d7dc3bde28783e
Nayah75 <okoye0805@gmail.com>
2025-07-20 05:14:30 +0100

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+difference = number2 - number1
+product = number1 * number2
+print('Addition of number1 + number2 is:', sum)
+print('Substraction of number2 - number1 is:', difference)
+print('Multiplication of number1 * number2 is:', product)
